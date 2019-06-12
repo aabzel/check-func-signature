4a17480f0d35ab09c1eb435b89e7a4c42d565022
Alexander Barunin <aabzel@yandex.ru>
2019-06-12 16:36:51 +0300

    Update check_func_signature.h

--- check_func_signature.h
+++ check_func_signature.h
@@ -7,14 +7,24 @@
 #include <stdio.h>
 #include <string>
 
+
+
 using namespace std;
 
+int check_incule_file (string inputFileNameC, string inputFileNameH );
+int is_proto(string codeSnippetIn);
+int check_header(string inputFileNameH);
 void print_list_of_functions_proto(void);
-void check_header(string inputFileNameH);
 string  clean_fun_proto_right(string codeSnippetIn);
 string clean_fun_proto_left (string codeSnippetIn);
 string separate_func_prototype(string codeSnippetIn);
 void parse_c_file(string inputFileNameC);
-int main(int argc, char *argv[]);
+int func_name_extractor (string inStr, string* outStr);
+string load_file_to_string (string inputFileName);
+string discard_one_line_comments(string inputFileNameC);
+string remove_preproc ( string codeSnippetOut);
+void findAndReplaceAll(string & data, string toSearch, string replaceStr);
+
+
 
 #endif /* CHECK_FUNC_SIGNATURE_H */
